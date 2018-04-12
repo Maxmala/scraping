e48d3e5795a29734c3b9f5cc9984f6546e3aa82a
Maxmala <37975336+Maxmala@users.noreply.github.com>
2018-04-12 23:14:27 +0200

    Add files via upload

--- route_des_incubateurs/route_des_incubateurs.rb
+++ route_des_incubateurs/route_des_incubateurs.rb
@@ -1,12 +1,48 @@
 require 'rubygems'
-require 'nokogiri' 
-require 'open-uri'  
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
 
-def get_the_email_of_a_townhal_from_its_webpage(url)
-    page = Nokogiri::HTML(open("http://mon-incubateur.com/site_incubateur/incubateurs"))  
 
-   incubateur_list = page.css(' a.site_incubateur/incubateur/33entrepreneurs-bordeaux">33entrepreneurs</a>')[].text
-puts incubateur_list
+
+PAGE_URL = "http://mon-incubateur.com/site_incubateur/incubateurs"
+def incubator
+    incubator_name = [] #création des tableaux de données nom, code postal et site internet
+    incubator_zipcode = []
+    incubator_website = []
+    my_hash = Hash.new #création du hash
+
+    page = Nokogiri::HTML(open(PAGE_URL))
+
+    i = 0
+
+    page.css('tr td p span a').each do |name|
+        incubator_name << name.text #scrap des données nom des incubateurs
+    end
+
+    sites = page.css('a')
+    lien = sites.each{|link| print link['href'] + " "}
+    lien.each do |site| #scrap des sites internet
+        incubator_website << site.text
+    end
+
+    zip = page.css('a').text #scrap des données code postal
+        zip.each do |z| #scrap des sites internet
+        incubator_zipcode << z.text
+    end
+
+
+    size = incubator_name.length
+    while i < size do #boucle pour ranger tableaux dans le hash
+        my_hash[i][:name] = "#{incubator_name[i]}"
+         my_hash[i][:zipcode] = "#{incubator_zipcode[i]}"
+        my_hash[i][:website] = "#{incubator_website[i]}"
+        i += 1 #évolution indice du hash
+        my_hash << my_hash[i]
+    end
+    puts my_hash
 
 end
+
+incubator
